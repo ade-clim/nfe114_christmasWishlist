c8bfc9dd5cc1d8a7ad64af4418e4c7e054108d4d
ade-clim <a.declimmer@gmail.com>
2020-01-28 14:11:31 +0100

    ajout dynamique name user reserved gift

--- assets/js/pages/ListesPage.jsx
+++ assets/js/pages/ListesPage.jsx
@@ -4,6 +4,11 @@ import listeApi from "../services/listeApi";
 import listeItemsApi from "../services/listeItemsApi";
 import userApi from '../services/userApi'
 import jwtDecode from "jwt-decode";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGift } from '@fortawesome/free-solid-svg-icons'
+
+
+
 const ListesPage = ({match, history}) => {
 
     const {id} = match.params;
@@ -62,9 +67,21 @@ const ListesPage = ({match, history}) => {
         try {
             await listeApi.delete(id);
         }catch (error) {
+            console.log(error.response);
             setListes(originalListes);
         }
     };
+
+    // Supprimer une reservation de cadeaux en fonction de l'id
+    const handleDeleteReservedGift = (listeItem) => {
+
+        try{
+
+        }catch(error){
+            console.log(error.response);
+        }
+    };
+
 
     // Gestion du changement de page
     const handlePageChange = (page) => setCurrentPage(page);
@@ -87,19 +104,36 @@ const ListesPage = ({match, history}) => {
     const paginatedListes = PaginationListes.getData(filteredListes, currentPage, itemsPerPage);
 
 
-    // reserve l'item en fonction de l'id de l'utilisateur en session
-    const handleReservedItem = async (listeItem, item) => {
-        console.log(listeItem);
-        // on recuperer l'id de l'utilisateur en session dans le state
+    // Reserve l'item en fonction de l'id de l'utilisateur en session
+    const handleReservedItem = async (listeItem, listeId) => {
+        const originalListes = [...listes];
+        const copyModifListes = [...listes];
+
+        // on recupére l'id de l'utilisateur en session dans le state
         const idUserSession = userSession;
 
+        for(let i = 0; i < copyModifListes.length; i++) {
+            if (copyModifListes[i].id === listeId) {
 
-        const addReservedUser = {...listeItem, userItem: idUserSession};
-        console.log(addReservedUser);
-        await listeItemsApi.update(listeItem.id, addReservedUser);
+                for (let p = 0; p < copyModifListes[i].listeItems.length; p++) {
+                    if (copyModifListes[i].listeItems[p].id === listeItem.id) {
+                        originalListes[i].listeItems[p].userItem = idUserSession;
+                        setListes(copyModifListes);
+                    }
+                }
+            }
+        }
+        try{
+            // supprimer l'userItem de listes/listeItems/userItem pour mettre à jour le state, affichage dynamique
 
+            const addReservedUser = {...listeItem, userItem: idUserSession};
+            await listeItemsApi.update(listeItem.id, addReservedUser);
+        }catch(error){
+            console.log(error.response);
+            setListes(originalListes);
+        }
 
-    };
+    }
 
 
     return(
@@ -136,8 +170,20 @@ const ListesPage = ({match, history}) => {
                                             {e.item.description}
                                             {e.item.price}
 
-                                            {e.userItem && <span>{e.userItem.firstName}{e.userItem.lastName}</span> ||
-                                            <button className={"btn btn-sm button_liste text-white"}onClick={() => {handleReservedItem(e, e.item)}}>reserver</button>}
+                                            {/* si le cadeau est reserver alors afficher l'utilisateur et cacher le button de reservation */}
+                                            {e.userItem &&
+                                                <span className={"ml-5"}>
+                                                    <FontAwesomeIcon color={"green"} icon={faGift} size={"lg"}/>
+                                                    {e.userItem.firstName}{e.userItem.lastName}
+                                                    {userSession.id === e.userItem.id && <button onClick={() => {handleDeleteReservedGift(e)}}>X</button>}
+                                                </span>
+                                             ||
+                                                <button className={"btn btn-sm button_liste text-white"}onClick={() => {handleReservedItem(e, liste.id)}}>
+                                                    reserver
+                                                </button>
+                                            }
+
+
 
                                         </p>
                                         </>)})}
